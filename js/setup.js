1e086c4f0dc125c64e8e88907e7ab32438065785
Tom <mckeont@upenn.edu>
2022-11-26 19:33:41 -0500

    Initial commit

---
+++ js/setup.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {
+  center: [39.983219999796114, -75.15356557830536],
+  zoom: 18,
+});
+
+var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
